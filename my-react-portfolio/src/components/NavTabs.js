b5fc8aa768b202b41757496c051fc670dfac8f26
Mia Reid <miareid@Mias-iMac.local>
2023-07-22 22:52:38 -0400

    resume and contact form added

--- my-react-portfolio/src/components/NavTabs.js
+++ my-react-portfolio/src/components/NavTabs.js
@@ -34,10 +34,10 @@ function NavTabs({ currentPage, handlePageChange }) {
                     </li>
                     <li className="nav-item">
                         <a
-                        href="#contact"
-                        onClick={() => handlePageChange('Contact')}
+                        href="#about"
+                        onClick={() => handlePageChange('About')}
                         // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                        className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
                         >
                             Resume
                         </a>
@@ -49,7 +49,7 @@ function NavTabs({ currentPage, handlePageChange }) {
                         // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                         className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
                         >
-                            Resume
+                            Contact
                         </a>
                     </li>
                 </ul>
--- my-react-portfolio/src/components/pages/About.js
+++ my-react-portfolio/src/components/pages/About.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import '../styles/Header.css';
+
+import resumePDF from '../styles/Mia Reid - Marketing Manager, 2023.pdf';
+import resumePNG from '../styles/Mia Reid - Marketing Manager, 2023.png';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="container-body">
+    <div className="container">
+      <h2 className="h2-body display-5 fw-bold">Resume</h2>
+
+      <a href={resumePDF} download="Mia-Reid-Resume-Web-Dev-Marketing" target='_blank'>
+      <img src={resumePNG} className="card-img-top" alt="..."/>
+      <div className="card mb-3">
+        <div className="card-body h3-background">
+          <h5 className="h3-body card-title">My Resume</h5>
+        </div>
+      </div>
+      </a>
     </div>
+  </div>
   );
 }
